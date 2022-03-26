966e4f3eda31d2b685dd1f5a5b280d10cf20d45e
Roxza <39166624+Roxza@users.noreply.github.com>
2022-03-27 00:25:33 +0300

    music has been added.

--- components/Theme/toggle.jsx
+++ components/Theme/toggle.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Transition } from "@headlessui/react";
+
 import darkTheme from "../../hooks/dark";
 
 export default function Toggle() {
   const [scrolled, setScrolled] = React.useState(false);
+  const [music, musicSet] = React.useState(false);
   const [colorTheme, setTheme] = darkTheme();
   React.useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -19,6 +21,17 @@ export default function Toggle() {
       top: 0,
       behavior: "smooth",
     });
+  };
+  const musicPlayer = () => {
+    const element = document.getElementById("music");
+
+    if (music) {
+      musicSet(false);
+      element.pause();
+    } else {
+      musicSet(true);
+      element.play();
+    }
   };
   return (
     <Transition
@@ -36,12 +49,6 @@ export default function Toggle() {
         className="fixed bottom-5 right-5 flex flex-row space-x-2"
       >
         <div
-          onClick={ScrollTop}
-          className="h-12 w-12 rounded-full dark:text-white text-black text-center flex items-center justify-center space-x-3 group relative overflow-hidden transition-all duration-500 bg-gradient-to-tl dark:bg-[#101010] bg-gray-300 bg-size-200 bg-pos-0 hover:bg-pos-100 cursor-pointer"
-        >
-          <i className="fal fa-chevron-up" />
-        </div>
-        <div
           onClick={() => setTheme(colorTheme)}
           className="h-12 w-12 rounded-full dark:text-white text-black text-center flex items-center justify-center space-x-3 group relative overflow-hidden transition-all duration-500 bg-gradient-to-tl dark:bg-[#101010] bg-gray-300 bg-size-200 bg-pos-0 hover:bg-pos-100 cursor-pointer"
         >
@@ -52,6 +59,18 @@ export default function Toggle() {
                 : "fa-solid fa-sun-bright"
             }
           ></i>
+        </div>
+        <div
+          onClick={musicPlayer}
+          className="h-12 w-12 rounded-full dark:text-white text-black text-center flex items-center justify-center space-x-3 group relative overflow-hidden transition-all duration-500 bg-gradient-to-tl dark:bg-[#101010] bg-gray-300 bg-size-200 bg-pos-0 hover:bg-pos-100 cursor-pointer"
+        >
+          <i class={`fa ${music ? "fa-pause" : "fa-play"}`}></i>
+        </div>
+        <div
+          onClick={ScrollTop}
+          className="h-12 w-12 rounded-full dark:text-white text-black text-center flex items-center justify-center space-x-3 group relative overflow-hidden transition-all duration-500 bg-gradient-to-tl dark:bg-[#101010] bg-gray-300 bg-size-200 bg-pos-0 hover:bg-pos-100 cursor-pointer"
+        >
+          <i className="fal fa-chevron-up" />
         </div>
       </div>
     </Transition>
